5f07b161adf31a7f51d7bba90d36f86667ec6bea
STAROSTA Martin <starosta@qex.local>
2017-02-03 16:33:56 +0100

    Fixed Code Sniffer errors.

--- forum-plugin.php
+++ forum-plugin.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Forum Plugin for Wordpress
-Plugin URI: https://mayorsoft.eu
-Description: Plugin adds forum features to Wordpress site.
-Version: 1.0
-Author: Martin Starosta
-Author URI: https://mayorsoft.eu
-*/
+/**
+ * Plugin Name: Forum Plugin for Wordpress
+ * Plugin URI: https://mayorsoft.eu
+ * Description: Plugin adds forum features to Wordpress site.
+ * Version: 1.0
+ * Author: Martin Starosta
+ * Author URI: https://mayorsoft.eu
+ */
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -15,10 +15,12 @@ if ( ! defined( 'WPINC' ) ) {
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/fp.php';
 
+/**
+ * Run Forum plugin code
+ */
 function run_forum() {
-
 	$plugin = new Forum();
-
 }
+
 run_forum();
 
--- includes/fp.php
+++ includes/fp.php
@@ -1,88 +1,120 @@
 <?php
+/**
+ * Filename:  fp.php
+ *
+ * @author    Martin Starosta<martin.starosta83@gmail.com>
+ * @copyright 2017 Mayorsoft.eu
+ * @license   GPL
+ * @package   FP
+ * @see       https://kamforum.sk
+ */
 
+/**
+ * Main class for Forum Plugin
+ */
 class Forum {
 
-    public function __construct() {
-        register_activation_hook( __FILE__, array( $this, 'fp_activate' ) );
-        register_deactivation_hook( __FILE__, array( $this, 'fp_deactivate' ) );
+	/**
+	 * Class constructor
+	 */
+	public function __construct() {
+		register_activation_hook( __FILE__, array( $this, 'fp_activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'fp_deactivate' ) );
 
-        add_action('init', array($this,'fp_run'));
-        add_action('init', array($this,'fp_create_taxonomies'));
-    }
+		add_action( 'init', array( $this, 'fp_run' ) );
+		add_action( 'init', array( $this, 'fp_create_taxonomies' ) );
+	}
 
-    public function fp_run() {
-        /* Nothing implemented yet. */
-    }
+	/**
+	 * Function initialize plugin and call init functions.
+	 */
+	public function fp_run() {
+		/* Nothing implemented yet. */
+	}
 
-    public function fp_activate() {
-        echo 'Forum Plugin has been activated';
-    }
+	/**
+	 * Function is called on plugin activation.
+	 */
+	public function fp_activate() {
+		echo 'Forum Plugin has been activated';
+	}
 
-     public function fp_deactivate() {
-        echo 'Forum Plugin has been deactivated';
-    }
+	/**
+	 * Function is called on plugin deactivation.
+	 */
+	public function fp_deactivate() {
+		echo 'Forum Plugin has been deactivated';
+	}
 
-    /* Private functions */
-    public function fp_create_taxonomies() {
-        $this->create_customers_taxonomy();
-        $this->create_segments_taxonomy();
-    }
+	/**
+	 * Function creates taxonomies required for forum.
+	 */
+	public function fp_create_taxonomies() {
+		$this->create_customers_taxonomy();
+		$this->create_segments_taxonomy();
+	}
 
-    private function create_segments_taxonomy() {
-        $labels = array(
-            'name' => 'Segmenty',
-            'singular_name' => 'Segment',
-            'search_items' => 'Hľadať podľa segmentu',
-            'all_items' => 'Všetky segmenty',
-            'edit_item' => 'Upraviť segment',
-            'update_item' => 'Aktualizovať segment',
-            'add_new_item' => 'Pridať nový segment',
-            'menu_name' => 'Segmenty',
-            'view_items' => 'Zobraziť segmenty',
-            'popular_items' => 'Najčastejšie používané segmenty',
-            'separate_items_with_comas' => 'Oddeľte segmenty čiarkou',
-            'add_or_remove_item' => 'Pridať alebo odobrať segment',
-            'choose_from_most_used' => 'Vybrať z najčastejšie používaných',
-            'not_found' => 'Segment sa nenašiel',
-        );
+	/**
+	 * Function registers segments taxonomy.
+	 */
+	private function create_segments_taxonomy() {
+		$labels = array(
+			'name' => 'Segmenty',
+			'singular_name' => 'Segment',
+			'search_items' => 'Hľadať podľa segmentu',
+			'all_items' => 'Všetky segmenty',
+			'edit_item' => 'Upraviť segment',
+			'update_item' => 'Aktualizovať segment',
+			'add_new_item' => 'Pridať nový segment',
+			'menu_name' => 'Segmenty',
+			'view_items' => 'Zobraziť segmenty',
+			'popular_items' => 'Najčastejšie používané segmenty',
+			'separate_items_with_comas' => 'Oddeľte segmenty čiarkou',
+			'add_or_remove_item' => 'Pridať alebo odobrať segment',
+			'choose_from_most_used' => 'Vybrať z najčastejšie používaných',
+			'not_found' => 'Segment sa nenašiel',
+		);
 
-        register_taxonomy(
-            'segments',
-            'post',
-            array(
-                'label' => __('Segment'),
-                'hierarchical' => true,
-                'labels' => $labels,
-            )
-        );
-    }
+		register_taxonomy(
+			'segments',
+			'post',
+			array(
+				'label' => __( 'Segment' ),
+				'hierarchical' => true,
+				'labels' => $labels,
+			)
+		);
+	}
 
-    private function create_customers_taxonomy() {
-        $labels = array(
-            'name' => 'Zákazníci',
-            'singular_name' => 'Zákazník',
-            'search_items' => 'Hľadať podľa zákazníka',
-            'all_items' => 'Všetci zákazníci',
-            'edit_item' => 'Upraviť zákazníka',
-            'update_item' => 'Aktualizovať zákazníka',
-            'add_new_item' => 'Pridať nového zákazníka',
-            'menu_name' => 'Zákazníci',
-            'view_items' => 'Zobraziť zákazníka',
-            'popular_items' => 'Najčastejšie používaný zákazníci',
-            'separate_items_with_comas' => 'Oddeľte zákazníkov čiarkou',
-            'add_or_remove_item' => 'Pridať alebo odobrať zákazníka',
-            'choose_from_most_used' => 'Vybrať z najčastejšie používaných',
-            'not_found' => 'Zákazník sa nenašiel',
-        );
+	/**
+	 * Function registers customer taxonomy.
+	 */
+	private function create_customers_taxonomy() {
+		$labels = array(
+			'name' => 'Zákazníci',
+			'singular_name' => 'Zákazník',
+			'search_items' => 'Hľadať podľa zákazníka',
+			'all_items' => 'Všetci zákazníci',
+			'edit_item' => 'Upraviť zákazníka',
+			'update_item' => 'Aktualizovať zákazníka',
+			'add_new_item' => 'Pridať nového zákazníka',
+			'menu_name' => 'Zákazníci',
+			'view_items' => 'Zobraziť zákazníka',
+			'popular_items' => 'Najčastejšie používaný zákazníci',
+			'separate_items_with_comas' => 'Oddeľte zákazníkov čiarkou',
+			'add_or_remove_item' => 'Pridať alebo odobrať zákazníka',
+			'choose_from_most_used' => 'Vybrať z najčastejšie používaných',
+			'not_found' => 'Zákazník sa nenašiel',
+		);
 
-        register_taxonomy(
-            'customers',
-            'post',
-            array(
-                'label' => __('Customer'),
-                'hierarchical' => true,
-                'labels' => $labels,
-            )
-        );
-    }
+		register_taxonomy(
+			'customers',
+			'post',
+			array(
+				'label' => __( 'Customer' ),
+				'hierarchical' => true,
+				'labels' => $labels,
+			)
+		);
+	}
 }
